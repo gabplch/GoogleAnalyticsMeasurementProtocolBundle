b4ff6943a6ddcdeec4550d98097eab71982657d7
Yarysh Vlad <yarysh.vlad@gmail.com>
2023-01-22 18:32:57 +0200

    add bundle file

--- src/GoogleAnalyticsMeasurementProtocolBundle.php
+++ src/GoogleAnalyticsMeasurementProtocolBundle.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\DependencyInjection\Compiller\RegisterMeasurementProtocolPass;
+use App\DependencyInjection\Compiler\RegisterMeasurementProtocolPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
