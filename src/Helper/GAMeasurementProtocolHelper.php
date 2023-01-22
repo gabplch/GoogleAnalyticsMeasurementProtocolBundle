233475022ff1d79d2c74303a0f3022b2a1a34e70
Yarysh Vlad <yarysh.vlad@gmail.com>
2023-01-22 18:32:57 +0200

    add helper to generate path

---
+++ src/Helper/GAMeasurementProtocolHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper;
+
+class GAMeasurementProtocolHelper
+{
+    public const MEASUREMENT_PROTOCOL_HOST = 'https://www.google-analytics.com';
+    public const MEASUREMENT_PROTOCOL_PATH = '/mp/collect';
+    public const MEASUREMENT_PROTOCOL_DEBUG_PATH = '/debug';
+
+    public static function getMeasurementProtocolConnectionPath(bool $isDebug): string
+    {
+        $path = self::MEASUREMENT_PROTOCOL_HOST;
+
+        if ($isDebug) {
+            $path = \sprintf('%s%s', $path, self::MEASUREMENT_PROTOCOL_DEBUG_PATH);
+        }
+
+        $path = \sprintf('%s%s', $path, self::MEASUREMENT_PROTOCOL_PATH);
+
+        return $path;
+    }
+}
