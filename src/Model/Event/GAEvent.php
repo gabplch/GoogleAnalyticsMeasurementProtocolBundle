318475f5522475322c4e1acc7e964d66cd2297be
Yarysh Vlad <yarysh.vlad@gmail.com>
2023-02-06 17:33:12 +0200

    events can have few parameters

--- src/Model/Event/GAEvent.php
+++ src/Model/Event/GAEvent.php
@@ -4,12 +4,31 @@ declare(strict_types=1);
 
 namespace Gabplch\GoogleAnalyticsMeasurementProtocolBundle\Model\Event;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class GAEvent implements GAEventInterface
 {
-    public function __construct(
-        private string $name,
-        private GAEventParameterInterface $parameter,
-    ) {
+    #[Assert\Sequentially(
+        constraints: [
+            new Assert\NotBlank(),
+            new Assert\Length(min: 1),
+        ]
+    )]
+    private string $name;
+
+    #[Assert\Sequentially(
+        constraints: [
+            new Assert\NotBlank(),
+            new Assert\Count(
+                min: 1,
+                max: 25,
+            ),
+        ]
+    )]
+    private array $parameters = [];
+
+    public function __construct(string $name) {
+        $this->name = $name;
     }
 
     public function getName(): string
@@ -24,14 +43,29 @@ class GAEvent implements GAEventInterface
         return $this;
     }
 
-    public function getParameter(): GAEventParameterInterface
+    /**
+     * @return GAEventParameterInterface[]
+     */
+    public function getParameters(): array
     {
-        return $this->parameter;
+        return $this->parameters;
     }
 
-    public function setParameter(GAEventParameterInterface $parameter): self
+    public function addParameter(GAEventParameterInterface $parameter): self
     {
-        $this->parameter = $parameter;
+        $this->parameters[] = $parameter;
+
+        return $this;
+    }
+
+    /**
+     * @param GAEventParameterInterface[] $parameters
+     *
+     * @return $this
+     */
+    public function setParameters(array $parameters): self
+    {
+        $this->parameters = $parameters;
 
         return $this;
     }
